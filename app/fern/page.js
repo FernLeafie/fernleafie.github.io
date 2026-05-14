307a049eb4d7ca068cf9eb24125646f33c03fd0e
Fernleafie <fernleafie@gmail.com>
2026-05-14 11:24:35 +0200

    Start of customisation

---
+++ app/fern/page.js
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+function Home() {
+  return (
+    <>
+      <Header/>
+      <p>meow, mrrp, meow</p>
+    </>
+  )
+}
+
+export default Home;
---
+++ components/Header.js
@@ -0,0 +1,9 @@
+function Header(props) {
+  return <nav>
+    <p>Snowleafie</p>
+    <p>Fern</p>
+    <p>Lily</p>
+    <p>Articles</p>
+  </nav>
+}
+export default Header;
