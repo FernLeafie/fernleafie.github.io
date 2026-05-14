f246578c9f23ce22511c112f6a4d8f22dac3152a
Fernleafie <fernleafie@gmail.com>
2026-05-14 23:28:36 +0200

    First implementation of container

--- app/fern/page.js
+++ app/fern/page.js
@@ -1,12 +1,23 @@
 import Header from "@/components/Header";
+import Container from "@/components/Container";
+import Image from "next/image";
 
 function Home() {
   return (
     <>
-      <Header/>
+      <Header />
+      <Container title={{ name: <span>fern snowleafie</span> }}>
+        <Image
+          src="/assets/profiles/fern.webp"
+          alt="Fern's profile picture"
+          width={200}
+          height={200}
+        />
+      </Container>
       <p>meow, mrrp, meow</p>
     </>
-  )
+  );
 }
 
 export default Home;
+
