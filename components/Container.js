f4c581048b0d18c005ef06e8e5ba1b9d013d8580
Fernleafie <fernleafie@gmail.com>
2026-05-15 00:01:37 +0200

    Container now has right and left alignment

--- components/Container.js
+++ components/Container.js
@@ -1,10 +1,11 @@
 function Container({ title, children }) {
   return (
-    <div className="container">
-      <div className="container-header">{title.name}</div>
-      <div className="container-content">
-        {children}
+    <div className="container" style={{ width: "fit-content" }}>
+      <div className="container-header">
+        <span>{title.left}</span>
+        <span>{title.right}</span>
       </div>
+      <div className="container-content">{children}</div>
     </div>
   );
 }
