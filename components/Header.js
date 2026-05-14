7437e0fc587e07d250c8733c98cd51ad4f9f6942
Fernleafie <fernleafie@gmail.com>
2026-05-14 22:44:12 +0200

    Start of header styling

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,16 @@
 function Header(props) {
   return (
-    <nav>
-      <span style={{fontStyle: 'italic'}}>
+    <nav className="header">
+      <span className="button" style={{fontStyle: 'italic'}}>
         <a href="/">Snowleafie</a>
       </span>
-      <span>
+      <span className="button">
         <a href="/fern/">Fern</a>
       </span>
-      <span>
+      <span className="button">
         <a href="/lily/">Lily</a>
       </span>
-      <span>
+      <span className="button">
         <a href="/articles/">Articles</a>
       </span>
     </nav>
