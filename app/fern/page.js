684201bd04e61d9c7789700cfa8ebf3b194a4d4b
Fernleafie <fernleafie@gmail.com>
2026-05-15 01:17:49 +0200

    Nicer layouting :3

--- app/fern/page.js
+++ app/fern/page.js
@@ -39,7 +39,7 @@ function Home() {
               right: "x",
             }}
           >
-            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
             faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
             pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
             tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
@@ -72,7 +72,7 @@ function Home() {
             vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
             nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel
             class aptent taciti sociosqu. Ad litora torquent per conubia nostra
-            inceptos himenaeos.
+            inceptos himenaeos.</p>
           </Container>
         </div>
       </div>
