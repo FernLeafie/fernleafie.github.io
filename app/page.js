9f7358a815e8d8093786e6817929309b5a8ed6ca
Fernleafie <fernleafie@gmail.com>
2026-05-15 20:42:40 +0200

    Woops, actually new homepage!

--- app/page.js
+++ app/page.js
@@ -7,64 +7,94 @@ import Icon from "@/components/Icon";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <Header />
+      <div className="page">
+        <div className="page-content">
+          <div className="row row-center">
+            <a href="/fern/">
+              <Profile
+                name={"Fern Snowleafie"}
+                image={"fern.webp"}
+                links={
+                  <>
+                    {" "}
+                    <Logo
+                      logo="discord"
+                      link="https://discord.com/users/582201423748399115"
+                    />
+                    <Logo
+                      logo="bluesky"
+                      link="https://bsky.app/profile/fernleaf.bsky.social"
+                    />
+                    <Logo logo="github" link="https://github.com/FernLeafie" />
+                    <Logo
+                      logo="anilist"
+                      link="https://anilist.co/user/fernleafie/"
+                    />
+                    <Logo
+                      logo="hardcover"
+                      link="https://hardcover.app/@fernleafie"
+                    />
+                    <Logo
+                      logo="pronounscc"
+                      link="https://pronouns.cc/@fernleafie"
+                    />
+                  </>
+                }
+              />
+            </a>
+            <a href="/lily/">
+              <Profile
+                name={"Lily Snowleafie"}
+                image={"lily.webp"}
+                links={
+                  <>
+                    {" "}
+                    <Logo
+                      logo="discord"
+                      link="https://discord.com/users/680873600181469194"
+                    />
+                    <Logo logo="github" link="https://github.com/CodedKyoko" />
+                    <Logo
+                      logo="anilist"
+                      link="https://anilist.co/user/TheKyoko/"
+                    />
+                    <Logo
+                      logo="hardcover"
+                      link="https://hardcover.app/@TheKyoko"
+                    />
+                    <Logo
+                      logo="pronounscc"
+                      link="https://pronouns.cc/@TheKyoko"
+                    />
+                  </>
+                }
+              />
+            </a>
+          </div>
+          <Container
+            title={{
+              left: (
+                <>
+                  <Icon icon="file" />
+                  <span>Cute homepage pending.md</span>
+                  <span className="info">
+                    <Icon icon="file" />
+                    <span>Markdown</span>
+                    <Icon icon="storage" />
+                    <span>0 KiB</span>
+                  </span>
+                </>
+              ),
+
+              right: <Icon icon="close" />,
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <p></p>
+          </Container>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
