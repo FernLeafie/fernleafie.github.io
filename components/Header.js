894c6dfdb1452d353ba6aa16388b9607a2331e68
Fernleafie <fernleafie@gmail.com>
2026-05-15 20:55:18 +0200

    Added slashes to header, because it looks cool and techy :3

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,18 @@
 function Header(props) {
   return (
     <nav className="header">
-      <span className="button" style={{fontStyle: 'italic'}}>
+      <span className="button" style={{ fontStyle: "italic" }}>
         <a href="/">Snowleafie</a>
       </span>
+      /
       <span className="button">
         <a href="/fern/">Fern</a>
       </span>
+      /
       <span className="button">
         <a href="/lily/">Lily</a>
       </span>
+      /
       <span className="button">
         <a href="/articles/">Articles</a>
       </span>
